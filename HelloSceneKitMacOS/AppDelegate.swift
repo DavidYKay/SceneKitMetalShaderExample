01ce16b78fd31e11daa252bef2b02120d76e1b61
David Young-chan Kay <davidykay@gmail.com>
2016-09-01 01:20:38 -0700

    Initial Commit

---
+++ HelloSceneKitMacOS/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  HelloSceneKitMacOS
+//
+//  Created by David Young-chan Kay on 2016-09-01.
+//  Copyright (c) 2016 David Young-chan Kay. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    @IBOutlet weak var window: NSWindow!
+    
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+    
+}
